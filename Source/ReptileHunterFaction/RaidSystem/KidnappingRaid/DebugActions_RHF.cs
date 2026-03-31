524b5d853fef555b09d037bb7abafa8d8d3b5be2
KahirDragoon <markus.deeken@gmail.com>
2026-04-01 00:02:49 +0200

    made Snakes optional plus raid error fix

--- Source/ReptileHunterFaction/RaidSystem/KidnappingRaid/DebugActions_RHF.cs
+++ Source/ReptileHunterFaction/RaidSystem/KidnappingRaid/DebugActions_RHF.cs
@@ -9,10 +9,12 @@ public static class DebugActions_RHF
     [DebugAction("RHF", "Kidnapping raid", allowedGameStates = AllowedGameStates.PlayingOnMap)]
     private static void TriggerKidnappingRaid()
     {
+        var map = Find.CurrentMap;
         DefDatabase<IncidentDef>.GetNamed("RHF_KidnappingRaid").Worker.TryExecute(new IncidentParms
         {
-            target = Find.CurrentMap,
-            forced = true
+            target = map,
+            forced = true,
+            points = StorytellerUtility.DefaultThreatPointsNow(map)
         });
     }
 }
