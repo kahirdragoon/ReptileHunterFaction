37084cb1ef1b071e4c48e1d54a5191d918181d57
KahirDragoon <markus.deeken@gmail.com>
2026-04-01 15:38:08 +0200

    support for barbarian style

--- Source/ReptileHunterFaction/WorldComp_RHFSkulls.cs
+++ Source/ReptileHunterFaction/WorldComp_RHFSkulls.cs
@@ -8,23 +8,25 @@ namespace ReptileHunterFaction;
 /// Each entry is the short name of the victim (used to call CompHasSources.AddSource
 /// when placing Skull items or Skullspike buildings during settlement map generation).
 /// </summary>
-public class WorldComp_RHFSkulls : WorldComponent
+public class WorldComp_SpoilsOfBattle(World world) : WorldComponent(world)
 {
-    public List<string> skulls = [];
+    public List<string> skulls  = [];
+    public List<Corpse> corpses = [];
 
     public int SkullCount => skulls.Count;
 
-    public WorldComp_RHFSkulls(World world) : base(world) { }
-
     public void AddSkull(string pawnName) => skulls.Add(pawnName);
+    public void AddCorpse(Corpse corpse)  => corpses.Add(corpse);
 
-    public static WorldComp_RHFSkulls? Get() =>
-        Find.World.GetComponent<WorldComp_RHFSkulls>();
+    public static WorldComp_SpoilsOfBattle? Get() =>
+        Find.World.GetComponent<WorldComp_SpoilsOfBattle>();
 
     public override void ExposeData()
     {
         base.ExposeData();
-        Scribe_Collections.Look(ref skulls, "skulls", LookMode.Value);
-        skulls ??= [];
+        Scribe_Collections.Look(ref skulls,  "skulls",  LookMode.Value);
+        Scribe_Collections.Look(ref corpses, "corpses", LookMode.Deep);
+        skulls  ??= [];
+        corpses ??= [];
     }
 }
